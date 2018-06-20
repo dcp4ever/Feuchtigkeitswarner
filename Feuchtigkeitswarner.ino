a5a861538c1c36e2545b5ffca01626e1d0d8455d
dcp4ever <dcp4ever@gmail.com>
2018-06-20 11:38:57 +0200

    Powersaving
    Schlafmodus hinzugefügt, um Strom zusparen

--- Feuchtigkeitswarner.ino
+++ Feuchtigkeitswarner.ino
@@ -1,4 +1,7 @@
 
+#include <avr/sleep.h>
+#include <avr/power.h>
+
 // erstellen einiger Variablen
 int feuchteLevel = 0;
 int port = 0;
@@ -8,17 +11,47 @@ const int sensorCount = 5;
 const int sensors[sensorCount] = {A1, A2, A3, A4, A5};
 const int statusLED[] = {13,21,11,10,9};
 const String zustand[] = {"nA", "trocken", "feucht", "nass"};
+volatile int toggle = 0;
 
 #define thresh_trocken 800
 #define thresh_nass 400
-
 #define trocken 1
 #define nass 3
 #define feucht 2
 
 
+ISR(TIMER1_OVF_vect)
+  /* Timer1 Interrupt service Routine */
+  {
+  if(toggle == 0)
+    toggle = 1;
+  }
 
- 
+void enter_sleep(void)
+  /* Arduino schlafen legen */
+  {
+    Serial.println("Entering Sleepmode");
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+
+  /* Weil diesmal nicht der tiefste Schlafmodus gewaehlt wurde,
+   * koennen unbenutzte Komponenten noch zusaetzlich abgeschaltet
+   * werden, um Energie zu sparen. Noch wichtiger: Die Komponenten
+   * koennten ggf. den Arduino ungewollt aufwecken.
+   */
+  power_adc_disable();    /* Analog-Eingaenge abschalten */
+  power_spi_disable();    /* SPI abschalten */
+  power_timer0_disable(); /* Timer0 abschalten */
+  power_timer2_disable(); /* Timer0 abschalten */
+  power_twi_disable();    /* TWI abschalten */
+
+  sleep_mode();
+
+  sleep_disable(); /* nach dem Schlafen, wird das Programm ab hier weiter ausgeführt*/
+  
+  power_all_enable();     /* Komponenten wieder aktivieren */
+  }
+
 // Bestimmen der Feuchtestaerke der gemessenen Stelle
 int best_feuchteLevel(int port)
 {
@@ -44,30 +77,36 @@ Serial.begin(9600);
     delay(50);
   }
 
+
+/* Timer konfigurieren, dass der Arduino für ca 4sek schläft */
+  TCCR1A = 0x00;  /* Normalbetrieb */
+  TCNT1 = 0x0000; /* Zaehler loeschen */  
+  TCCR1B = 0x05;  /* Prescaler: 1024 */
+  TIMSK1 = 0x01;  /* Timer-Interrupt einschalten */
+
 }
  
 void loop()
 {
-
-
-for (int i = 0; i < sensorCount; i++) {
-    Serial.print("sensor: ");
-    Serial.print(i);
-    Serial.print("-");
-    int reading = best_feuchteLevel(sensors[i]);
-    delay(50);
-    Serial.println(zustand[reading]);  
-   if (reading == nass)     
-    digitalWrite(statusLED[i],HIGH);   
-   else
-      digitalWrite(statusLED[i],LOW);
-}
-      
-
+if(toggle==1) //für den Schlafmodus benötigter Toggle
+  {
+  toggle = 0;
+  for (int i = 0; i < sensorCount; i++) {
+      Serial.print("sensor: ");
+      Serial.print(i);
+      Serial.print("-");
+      int reading = best_feuchteLevel(sensors[i]);
+      delay(50);
+      Serial.println(zustand[reading]);  
+     if (reading == nass)     
+      digitalWrite(statusLED[i],HIGH);   
+     else
+        digitalWrite(statusLED[i],LOW);
+  }
 Serial.println("-----------");
-delay(1000);
+//delay(1000);
+enter_sleep();
+}
  
- //Hier springt der Loop in den oben angegebenen Programmabschnitt "Tasterstatus" und liest dort den gedrückten Taster aus.
-
 } //Loop beenden
 
